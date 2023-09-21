5c209e404c5771011ac4462db2006633d0fde1a2
tzchrzanowski <tzchrzanowski@gmail.com>
2023-09-21 10:55:10 +0800

    basic godot 2d scene setup

---
+++ basicNode.gd
@@ -0,0 +1,14 @@
+extends Node
+
+func print_param(param):
+	print(param);
+		
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print_param("asd")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
