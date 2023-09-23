5490ab82ef2d07cd53eb98570e9ac00419c76a9d
tzchrzanowski <tzchrzanowski@gmail.com>
2023-09-23 13:58:46 +0800

    added background image, and placed images into resources folder

--- Player.gd
+++ Player.gd
@@ -1,12 +1,10 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 400.0
+const JUMP_VELOCITY = -500.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 
 func _physics_process(delta):
 	# Add the gravity.
