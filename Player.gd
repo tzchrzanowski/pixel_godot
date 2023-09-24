0ad3205ab46d89e6470b6f2c5efca38838305d95
tzchrzanowski <tzchrzanowski@gmail.com>
2023-09-24 11:38:40 +0800

    changed textures, and added idle animation

--- Player.gd
+++ Player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const SPEED = 400.0
+const SPEED = 350.0
 const JUMP_VELOCITY = -500.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -12,9 +12,11 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	elif Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+	else:
+		$animatedSprite2D.play("idle");
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
