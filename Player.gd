8f552ac2de0643a8a59b3469d20df520ca9385cf
tzchrzanowski <tzchrzanowski@gmail.com>
2023-09-25 14:04:18 +0800

    added jump sprites animation

--- Player.gd
+++ Player.gd
@@ -6,18 +6,30 @@ const JUMP_VELOCITY = -500.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func get_move_direction():
+	var direction = 0
+	if Input.is_action_pressed("new_left"):
+		direction = -1;
+	elif Input.is_action_pressed("new_right"):
+		direction = 1;
+	else:
+		direction = Input.get_axis("ui_left", "ui_right");
+	return direction;
+			
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		$animatedSprite2D.play("jump");
 	# Handle Jump.
 	elif Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY	
+		velocity.y = JUMP_VELOCITY
+		$animatedSprite2D.play("jump");
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	
+	var direction = get_move_direction();
+		
 	if direction:
 		if direction == -1:
 			$animatedSprite2D.flip_h = true;
@@ -29,7 +41,6 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		$animatedSprite2D.play("idle");
-		
 		
 	
 	move_and_slide()
