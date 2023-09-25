e10ed3158560043263f608aa92d76678d74f1073
tzchrzanowski <tzchrzanowski@gmail.com>
2023-09-25 12:04:23 +0800

    added running animation

--- Player.gd
+++ Player.gd
@@ -10,19 +10,22 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
 	# Handle Jump.
 	elif Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	else:
-		$animatedSprite2D.play("idle");
+		velocity.y = JUMP_VELOCITY	
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	
 	if direction:
 		velocity.x = direction * SPEED
+		$animatedSprite2D.play("run");
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		$animatedSprite2D.play("idle");
+		
+		
+	
 	move_and_slide()
